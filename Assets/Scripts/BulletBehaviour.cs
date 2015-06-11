e52506f726876a9c2eb39313228ceadea3bb1861
jeanjsm <jean_silvajsm@hotmail.com>
2015-06-11 11:10:54 -0400

    Commit Inicial

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class BulletBehaviour : MonoBehaviour {
+
+	public float speed;
+	public int damage;
+
+	// Player
+	private PlayerBehaviour player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType(typeof(PlayerBehaviour)) as PlayerBehaviour;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.forward * speed * Time.deltaTime);
+	}
+	
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.collider.tag == "Player"){
+			Debug.Log("Entrou");
+			player.ApplyDamage(damage);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public Transform player;
+	public Transform aim;
+	public GameObject bullet;
+
+	public float timeToShoot;
+
+	private float currentTimeToShoot;
+	private Quaternion rotation;
+
+	public float distanceFromCamera = 10.0f;
+
+	void Start(){
+		player = GameObject.Find("Player").transform;
+	}
+
+	void Update(){
+		rotation = Quaternion.LookRotation(transform.position - player.position, Vector3.forward);
+		rotation.x = 0.0f;
+		rotation.y = 0.0f;
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 8);
+		currentTimeToShoot += Time.deltaTime;
+		if(currentTimeToShoot > timeToShoot){
+			GameObject tempBullet = Instantiate(bullet.gameObject, aim.position, aim.rotation) as GameObject;
+			currentTimeToShoot = 0;
+		}
+	}
+}
